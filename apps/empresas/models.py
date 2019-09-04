9ff64591701fadb870f49e424ff7fb860e4606bb
wagiotti <wagiotti@gmail.com>
2019-09-04 12:23:34 -0300

    Create app funcionarios

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Empresa(models.Model):
     nome = models.CharField(max_length=200, help_text="Nome da empresa")
 
-    def __str(self):
+    def __str__(self):
         return self.nome
 
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.empresas',
+    'apps.funcionarios',
+
 ]
 
 MIDDLEWARE = [
