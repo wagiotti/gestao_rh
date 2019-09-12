d76fabfe17b674ae2bdd9847856db5fe357c93b6
wagiotti <wagiotti@gmail.com>
2019-09-12 11:51:33 -0300

    Update registro_horas_extra

--- apps/registro_hora_extra/forms.py
+++ apps/registro_hora_extra/forms.py
@@ -4,6 +4,7 @@ from apps.funcionarios.models import Funcionario
 
 
 class RegistroHoraExtraForm(ModelForm):
+    """ Sobrescrevendo o metodo __init__ para filtrar os funcionarios somente da empresa que pertence """
     def __init__(self, user, *args, **kwargs):
         super(RegistroHoraExtraForm, self).__init__(*args, **kwargs)
         self.fields['funcionario'].queryset = Funcionario.objects.filter(empresa = user.funcionario.empresa)
